29120c8a26eeca5dca700d93a01e9d116c35d2b0
D5BU <d00005ji@gmail.com>
2026-06-09 23:07:43 +0530

    Added tar bzip2 extraction logic for Busybox

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ mkdir -p "${WORKSPACE_DIR}/dist"
 mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
 wget -q --show-progress "https://busybox.net/downloads/busybox-1.36.1.tar.bz2"
+
+tar -xf busybox-1.36.1.tar.bz2
+cd busybox-1.36.1
