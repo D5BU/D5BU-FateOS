c73cf9f8b6229155fce8641c26d040ff9486bf35
D5BU <d00005ji@gmail.com>
2026-06-09 23:07:44 +0530

    Added sed rule to enforce CONFIG_STATIC in config

--- build.sh
+++ build.sh
@@ -10,3 +10,4 @@ wget -q --show-progress "https://busybox.net/downloads/busybox-1.36.1.tar.bz2"
 tar -xf busybox-1.36.1.tar.bz2
 cd busybox-1.36.1
 make defconfig
+sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/' .config
